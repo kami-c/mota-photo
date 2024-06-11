2a687e4358fed583c0a62ea4b61044343faa90ce
kami-c <150251831+kami-c@users.noreply.github.com>
2024-06-11 10:21:48 +0200

    Update contact.js
    - Amélioration de la fonction de gestion d'ouverture et fermeture de la modale
    - Ajout de la fonction de récupération de la référence de la photo en cours

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,49 @@
-/* ---------------------------- FENÊTRE MODALE ---------------------------- */
-function openCloseModal() { 
+// --------- >>> GESTIONNAIRE DE LA MODALE DE CONTACT <<< -------------- //
 
-    // >>> VARIABLE 
-    const modalLink = $('.modale')
+// >>> FONCTION D'OUVERTURE/FERMETURE DE LA MODALE
 
-    /// >>> GESTION DE LA FENÊTRE MODALE
+function openCloseModal() {  
+    // >>> REQUÊTE AJAX pour récupérer "contact.php"
+    $.ajax({
+        url: 'contact',
+        dataType: 'html',
+        success: function(data) {},
+
+        // SI PROBLÈME AVEC REQUÊTE
+        error: function(xhr, status, error) {
+            console.error('Erreur lors du chargement de contact.php : ' + status + ' - ' + error);
+        }
+    });
+
+    // >>> ÉCOUTEUR D'ÉVÈNEMENTS sur la class .modale : au clic
     $(".modale").on("click", function(e) {
+        // BLOCAGE DU RECHARGEMENT DE LA PAGE
         e.preventDefault();
 
-        // >>> RÉCUPÉRATION DE "contact.php"
-        $.ajax({
-            url: 'contact',
-            dataType: 'html',
-            success: function(data) {
-                const contact = data;
-
-                // >>> OUVERTURE LA MODALE
-                $(".modal").fadeIn();
-            },
-        });
-
-        // >>> GESTION DE FERMETURE DE LA MODALE
-        $(window).on("click", function(event) {
-            if ($(event.target).is(".modal")) {
-                $(".modal").fadeOut();
-            }
-        });
+        // OUVERTURE DE LA MODALE avec animation en fadeIn
+        $(".modal").fadeIn();
     });
+
+    // >>> ÉCOUTEUR D'ÉVÈNEMENTS des clics sur la fenêtre en dehors de la modale
+    $(window).on("click", function(event) {
+        // CONDITION pour vérifier SI l'élément cliqué à la class .modal (width : 100% donc présent sur toute la fenêtre)
+        if ($(event.target).is(".modal")) {
+            // FERMETURE DE LA MODALE avec une animation en fadeOut
+            $(".modal").fadeOut();
+        }
+    });
+    
 }
+
+// >>> FONCTION D'AJOUT DE LA RÉFÉRENCE DE LA PHOTO EN COURS
+function addRef() {  
+    // RÉCUPÉRATION DE LA RÉFÉRENCE PHOTO dans le post en cours
+    const currentReference = $('.reference').text();
+
+    // CHAMP DE RÉFÉRENCE PHOTO DU FORMAULAIRE SÉLECTIONNÉ
+    const reference = $('.reference-field');
+    
+    // AJOUT DE LA RÉFÉRENCE DANS LE FORMULAIRE au champ dédié
+    reference.val(currentReference);
+}
+
