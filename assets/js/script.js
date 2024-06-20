d6962a83011b0f38c450455f8a114ebb4df4ff82
kami-c <150251831+kami-c@users.noreply.github.com>
2024-06-20 09:25:05 +0200

    Update script.js
    Ajout des commentaires + scripts à charger (filters.js et lightbox.js)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+// >>> jQuery <<< //
 ( function( $ ) {
+
+    // >>> Exécution après chargement du complet du DOM
     $(() => {
+        
         // >>> EXCÉCUTION DES FONCTIONS DE LA MODALE DE CONTACT <<< //
         // >>> cf. contact.js
         openCloseModal()
         addRef()
+        
+        // >>> FILTRES
+        filters()
+        console.log("Filters OK")
+
+        // >>> LIGHTBOX
+        Lightbox.init()
+        console.log("Lightbox OK")
     })
 
 } )( jQuery )
