71e08550473c2f763ab22a30449850066e4bca29
kami-c <150251831+kami-c@users.noreply.github.com>
2024-06-20 09:37:48 +0200

    Ajout des filtres

--- home.php
+++ home.php
@@ -5,60 +5,100 @@
  */
 get_header(); ?>
 
-<div class="hero-header">
-    <h1 class="hero-header__title">PHOTOGRAPHE EVENT</h1>
-
-<? // >>> GESTIONNAIRE HERO-HEADER <<<
-
-    // ARGUMENTS DE RÉCUPÉRATION DES PHOTOS
-    $args = array(
-        'post_type'        => 'photos',
-        'posts_per_page'   => 1,
-        'orderby'          => 'rand',
-        'tax_query'        => array(
-            array(
-                'taxonomy' => 'format',
-                'field'    => 'slug',
-                'terms'    => 'paysage',
-            ),
-        ),
-    );
-    // EXÉCUTION DE LA REQUÊTE WP QUERY
-    $photo_query = new WP_Query($args);
-
-    // RÉCUPÉRATION DES URLS DES IMAGES DANS UN TABLEAU
-    $image_url = array();
-
-    // VÉRIFICATION S'IL Y A DES POSTS À AFFICHER
-    if ($photo_query->have_posts()) :
-        // VÉRIFICATION DANS UNE BOUCLE S'IL Y A DES POSTS À CHAQUE ITÉRATION  
-        while ($photo_query->have_posts()) : 
-          // PRÉPARATION DES POSTS
-          $photo_query->the_post();
-
-            // GESTIONNAIRE D'AFFICHAGE D'IMAGE DU HERO HEADER
-            if (has_post_thumbnail()) {
-                $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-                $image_url = apply_filters('the_post_thumbnail_url', $image_url); 
-                $image_url = '<img src="' . esc_url($image_url) . '" alt="' . esc_attr(get_the_title()) . '">';
-                $image_url = preg_replace( // ajout de la class "hero-header__img"
-                    '/<img(.*?)(src="[^"]*")(.*?)>/',
-                    '<img class="hero-header__img" $2$1$3>',
-                    $image_url
-                );
-
-                echo $image_url; // AFFICHAGE DU HERO-HEADER
-            } else {
-                echo 'Aucune image disponible.';
-            }
-        endwhile;
-        // RÉINITIALISATION DE LA REQUÊTE PRINCIPALE
-        wp_reset_postdata();
-    endif;
-?>
-</div>
-
 <main>
+    
+    <section class="hero-header">
+        <h1 class="hero-header__title">PHOTOGRAPHE EVENT</h1>
+        <?php // >>> IMAGE ALÉATOIRE EN FOND
+        get_template_part( 'templates_part/hero_header' ) ?>
+    </section>
+    
+    <section class="filter-container">
+        <div class="filter-container__category-format">
+            <div class="filter-container__wrapper">
+                <form 
+                action="<?php echo admin_url( 'admin-ajax.php' ); ?>"
+                method="post"
+                class="filter-container__form__categorie"
+                >
+                    <?php
+            			$taxonomy = 'categorie';
+            			$args = array(
+            				'taxonomy'   => $taxonomy,
+            				'hide_empty' => false,
+            				'parent'     => 0, // Récupère uniquement les termes parents
+            			);
+                    
+            			$terms = get_terms($args);
+
+                        $default_value = 'all'; // Valeur par défaut
+
+                        if (!empty($terms) && !is_wp_error($terms)) {
+                            // Si des termes existent, utilise le slug du premier terme comme valeur par défaut
+                            $default_value = esc_attr($terms[0]->term_id);
+                        }
+                    ?>
+
+                    <select 
+                    type="hidden"
+                    id="filter-container__categorie" 
+                    class="filter-container__categorie" 
+                    name="categorie"
+                    value="<?php echo $default_value; ?>"
+                    >
+                        <option value="all" selected>Catégories</option>
+            			<option value=""></option>
+            			
+                        <?php
+            			    if (!empty($terms) && !is_wp_error($terms)) {
+            			    	foreach ($terms as $term) {
+            			    		echo '<option class="option" id="' . $term->slug . '" value="' . esc_attr($term->term_id) . '">' . esc_html($term->name) . '</option>';
+            			    	}
+            			    }
+                        ?>
+            		</select>
+                    <input type="hidden" name="nonce" value="<?php echo wp_create_nonce( 'categorie_filters' ); ?>"> 
+                    <input type="hidden" name="action" value="categorie_filters">
+                </form>
+            </div>
+
+            <div class="filter-container__wrapper">
+                <?php // Formulaire de filtres de formats                
+                mota_photo_format_filter()?>
+            </div>
+        </div>
+
+        <?php //FILTRE DATES
+        echo do_shortcode('[date_filter]'); ?> 
+        <div class="filter-container__date">
+            <div class="filter-container__wrapper">
+                <form 
+                class="filter-container__form__date"
+                action="<?php echo admin_url('admin-ajax.php'); ?>"
+                method="post"
+                >
+                    <select id="filter-container__date" name="filter-container__date">
+                        <option value="" disabled selected>Trier par date</option>
+	            		<option value=""></option>
+                        <option class="option" value="DESC">à partir des plus récentes</option>
+                        <option class="option" value="ASC">à partir des plus anciennes</option>
+	            	</select>
+
+                	<input type="hidden" name="action" value="filter-container__date_action">
+	            	<?php wp_nonce_field( 'filter-container__date_nonce', 'filter-container__date_nonce_field' ); ?>
+	            </form>
+            </div>
+        </div>
+    </section>    
+
+    <!-- <section class="photos-display"></section> -->
+
+
+    <?php // >>> BLOC PHOTOS
+    get_template_part( 'templates_part/photos_block' );
+     ?>
+
+    <button class="supplement">Charger plus</button>
 </main>
 
 <?php get_footer(); ?>
