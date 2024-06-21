2abaa7472bd728eb4e616ec438c9e76f70724808
kami-c <150251831+kami-c@users.noreply.github.com>
2024-06-21 17:45:57 +0200

    Update script.js
    Ajout des fonctions de filtres et de la lightbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+// >>> jQuery <<< //
 ( function( $ ) {
+
+    // >>> Exécution après chargement du complet du DOM
     $(() => {
+        
         // >>> EXCÉCUTION DES FONCTIONS DE LA MODALE DE CONTACT <<< //
         // >>> cf. contact.js
         openCloseModal()
         addRef()
+
+        // >>> FILTERS
+        // >>> cf. filters.js
+        filters()
+
+        // >>> LIGHTBOX
+        // >>> cf. lightbox.js
+        lightbox()
     })
 
 } )( jQuery )
