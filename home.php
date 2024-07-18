7d053a22773659b1832143929ddfb2ff77b4b84b
kami-c <150251831+kami-c@users.noreply.github.com>
2024-07-18 16:33:12 +0200

    Amélioration de la section filter-container

--- home.php
+++ home.php
@@ -10,60 +10,56 @@ get_header(); ?>
         <?php mota_photo_hero_header_filter(); // >>> IMAGE ALÉATOIRE EN FOND ?>
     </section>
     <section class="filter-container">
-        <div class="filter-container__category-format">
-            <div class="filter-container__wrapper">
-                <form class="filter-container__form__categorie">
-                    <select id="filter-container__categorie" name="category" aria-label="Catégorie">
-                        <option value="all">Catégories</option>
-                        <?php
-                        $categories = get_terms(array(
-                            'taxonomy'      => 'categorie',
-                            'hide_empty'    => false,
-                            'parent'        => 0,
-                        ));
-                        
-                        echo '<option value="" disabled></option>';
-                        foreach ($categories as $category) {
-                            echo '<option value="' . esc_attr($category->slug) . '">' . esc_html($category->name) . '</option>';
-                        }
-                        ?>
-                    </select>
-                    <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('category_filters'); ?>">
-                </form>
-            </div>
-            <div class="filter-container__wrapper">
-                <form class="filter-container__form__format">
-                    <select id="filter-container__format" name="format" aria-label="Format">
-                        <option value="all">Formats</option>
-                        <?php 
-                        $formats = get_terms(array(
-                            'taxonomy'          => 'format',
-                            'hide_empty'         => false,
-                            'parent'             => 0,
-                        ));
+        <form
+        action="filters"
+        method="post"
+        id="filters"
+        class="filter-container__form"
+        aria-label="Filtres de tri">
+            <div class="filter-container__category-format">
+                <select id="filter-container__categorie" name="category" aria-label="Catégorie">
+                    <option value="all">Catégories</option>
+                    <?php
+                    $categories = get_terms(array(
+                        'taxonomy'      => 'categorie',
+                        'hide_empty'    => false,
+                        'parent'        => 0,
+                    ));
 
-                        echo '<option value="" disabled></option>';
-                        foreach ($formats as $term) {
-                                echo '<option value="' . esc_attr($term->slug) . '">' . esc_html($term->name) . '</option>';
-                        }
-                        ?>
-                    </select>
-                    <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('format_filters'); ?>">
-                </form>
+                    echo '<option value="" disabled></option>';
+                    foreach ($categories as $category) {
+                        echo '<option value="' . esc_attr($category->slug) . '">' . esc_html($category->name) . '</option>';
+                    }
+                    ?>
+                </select>
+                <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('category_filters'); ?>">
+
+                <select id="filter-container__format" name="format" aria-label="Format">
+                    <option value="all">Formats</option>
+                    <?php 
+                    $formats = get_terms(array(
+                        'taxonomy'          => 'format',
+                        'hide_empty'         => false,
+                        'parent'             => 0,
+                    ));
+
+                    echo '<option value="" disabled></option>';
+                    foreach ($formats as $term) {
+                            echo '<option value="' . esc_attr($term->slug) . '">' . esc_html($term->name) . '</option>';
+                    }
+                    ?>
+                </select>
+                <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('format_filters'); ?>">
             </div>
-        </div>
-        <div class="filter-container__date">
-            <div class="filter-container__wrapper">
-                <form class="filter-container__form__date">
-                    <select id="filter-container__date" name="date" aria-label="Date">
-                        <option value="all">Trier par date</option>
-                        <option value=""></option>
-                        <option value="desc">Du plus récent au plus ancien</option>
-                        <option value="asc">Du plus ancien au plus récent</option>
-	            	</select>
-	            </form>
+            <div class="filter-container__date">
+                <select id="filter-container__date" name="date" aria-label="Date">
+                    <option value="all">Trier par date</option>
+                    <option value=""></option>
+                    <option value="desc">Du plus récent au plus ancien</option>
+                    <option value="asc">Du plus ancien au plus récent</option>
+	    	    </select>
             </div>
-        </div>
+        </form>
     </section>
     <div class="photo-bloc">
         <div id="photo-container"></div>
